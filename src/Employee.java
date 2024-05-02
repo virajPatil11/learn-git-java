01236a27f3982784c5dfddf7d1071c745538b3fa
Viraj Patil <virajpatilvp7045@gmail.com>
2024-05-02 15:13:08 +0530

    Initial commit

---
+++ src/Employee.java
@@ -0,0 +1,38 @@
+public class Employee {
+    private int empId;
+    private String empName;
+    private String email;
+    private String password;
+
+    public int getEmpId() {
+        return empId;
+    }
+
+    public void setEmpId(int empId) {
+        this.empId = empId;
+    }
+
+    public String getEmpName() {
+        return empName;
+    }
+
+    public void setEmpName(String empName) {
+        this.empName = empName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
